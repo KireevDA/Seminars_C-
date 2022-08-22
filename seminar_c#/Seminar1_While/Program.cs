f8102db9af92eb8b4ae1dd5ecab2d1d50ca523c8
Kireev_Dmitry <dakireev@mail.ru>
2022-08-22 15:06:30 +0300

    Задача -N до N

---
+++ seminar_c#/Seminar1_While/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на вход одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+int num;
+Console.Write("Введите любое число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while(current <= num){
+    Console.Write(current + " ");
+    current++;
+}
+
