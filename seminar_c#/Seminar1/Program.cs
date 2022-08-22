59ec73d1dbe00d6fa81758cfb72d51c68fd49e83
Kireev_Dmitry <dakireev@mail.ru>
2022-08-22 11:55:22 +0300

    Решение задачи корень числа

---
+++ seminar_c#/Seminar1/Program.cs
@@ -0,0 +1,13 @@
+﻿int num;
+
+// Ввод данных
+Console.Write("Введите число ");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+// Вычисление квадрата
+int square = num * num;
+
+// Вывод ответа на экран
+Console.WriteLine("квадрат введенного числа: " + square);
+
