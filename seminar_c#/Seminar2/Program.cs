edac783b0efd1b1a7f1a3baae01f932af6dccc92
Kireev_Dmitry <dakireev@mail.ru>
2022-08-22 20:58:43 +0300

    Задачи 1 и 2

---
+++ seminar_c#/Seminar2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+/*
+int CutNumber(){
+    int num = new Random().Next(100, 1000);
+    Console.WriteLine("Number is - " + num);
+    int dec = num / 100;
+    int ed = num % 10;
+    int result = dec*10 + ed;
+    return result;                             
+}
+
+int number = CutNumber();
+Console.WriteLine("Cut number is - " + number);
+*/
+
+/*
+void CutNumber(){
+    int num = new Random().Next(100, 1000);
+    Console.WriteLine("Number is - " + num);
+    int dec = num / 100;
+    int ed = num % 10;
+    int result = dec*10 + ed;
+    Console.WriteLine("Cut number is - " + result);
+}
+CutNumber();
+*/
+
+// 2 факториала перемножить
+/*
+int Factorial(int num){
+    int current = 1;
+    int fact = 1;
+    while(current <= num){
+        fact = fact * current;
+        //fact *= current;
+        current++;
+    }
+     return fact;
+}
+
+int result = Factorial(12) * Factorial(10);
+Console.WriteLine("Your result is - " + result);
+*/
+
+
+
+
+
